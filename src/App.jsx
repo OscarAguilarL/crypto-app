8c341bee012b7942119e4e46db67c7945ddbb29d
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-03-03 19:01:08 -0600

    Primeros componentes de styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
-import React from 'react';
-
 import styled from '@emotion/styled';
+import imgCrypto from './img/imagen-criptos.png';
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media screen and (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
 
 const Heading = styled.h1`
   font-family: 'Lato', sans-serif;
   color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
 `;
 
 function App() {
-
   return (
-    <Heading>Desde App</Heading>
+    <Container>
+      <Imagen src={imgCrypto} alt="Imagenes de criptomonedas" />
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+      </div>
+    </Container>
   );
 }
 
-export default App
+export default App;
