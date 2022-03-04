2cd61646861161e05568b2b0a90e50d015c5e0a7
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-03-03 18:52:17 -0600

    Styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+import styled from '@emotion/styled';
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+`;
 
 function App() {
 
   return (
-    <h1>Desde App</h1>
-  )
+    <Heading>Desde App</Heading>
+  );
 }
 
 export default App
